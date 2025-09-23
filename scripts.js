20d6bc03b94ca4ad179b8329757f284918c1a9d4
DIRETIDE <51263046+DIRETIDE@users.noreply.github.com>
2025-09-23 09:44:04 +0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Пример</title>
+    </head>
+    <body>
+        <!-- Подключение в конце body — чтобы DOM успел загрузиться -->
+        <script src="scripts.js"></script>
+    </body>
+</html>
